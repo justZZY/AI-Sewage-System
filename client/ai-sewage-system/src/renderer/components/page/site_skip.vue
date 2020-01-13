1448305b5955a6a842569483bcb406dd001b776c
justZZY <895527626@qq.com>
2020-01-13 18:57:23 +0800

    新增站点数据, 启停存在bug

---
+++ client/ai-sewage-system/src/renderer/components/page/site_skip.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'site_skip',
+    data () {
+      this.$router.replace({
+        path: '@/components/page/site_watch',
+        name: 'site_watch'
+      })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
