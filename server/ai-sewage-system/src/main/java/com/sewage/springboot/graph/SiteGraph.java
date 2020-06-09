d4dbdef7be7fa87c3cbb98be0ffde002dc45d827
justZZY <895527626@qq.com>
2020-06-10 01:56:45 +0800

    check out

--- server/ai-sewage-system/src/main/java/com/sewage/springboot/graph/SiteGraph.java
+++ server/ai-sewage-system/src/main/java/com/sewage/springboot/graph/SiteGraph.java
@@ -3,6 +3,7 @@ package com.sewage.springboot.graph;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.sewage.springboot.Global;
+import com.sewage.springboot.util.FboxTools;
 import okhttp3.*;
 import org.apache.jena.base.Sys;
 import org.apache.jena.rdf.model.*;
@@ -40,17 +41,16 @@ public class SiteGraph {
     }
     // URI = [ApplicationDomainName]/[BaseOntology]/[DomainOntology]/[DeviceID]
     final String BASE_URI = "https://www.zm-iot-platform.com/";
-    final String API_BASE_URL = "http://fbcs101.fbox360.com/api/";
-    private String authorization;
     private InfModel infModel;
     private TDBPersistence tdbPersistence;
+    private FboxTools fboxTools = new FboxTools();
 
 
     /**
      * @desc 图谱模型构造
      */
     private Model createGraphModel () throws IOException {
-        JSONArray siteArray = getSiteData();
+        JSONArray siteArray = fboxTools.getSiteData();
         Model model = ModelFactory.createDefaultModel();
         model.setNsPrefix("iot", BASE_URI);
         Resource iot = model.createResource(BASE_URI + "IOT/");
@@ -82,7 +82,7 @@ public class SiteGraph {
             String siteNo = siteStr[siteStr.length - 1]; // 末尾的是boxNo用于检索下属设备
             String siteName = siteStr[siteStr.length - 2]; // 用于校验测试站点数据
             try {
-                JSONArray equipArray = getEquipData(siteNo);
+                JSONArray equipArray = fboxTools.getEquipData(siteNo);
                 equipArray.forEach(group -> {
                     // 在这里解析设备json数据
                     // BASE_URI/equip/name/id
@@ -123,72 +123,5 @@ public class SiteGraph {
         });
         // 此时我们已经建立了iot-site site-equip之间的关系
         return model;
-    }
-
-    /**
-     * @desc 用于服务启动时获取站点设备以及硬件设备数据
-     * @return 设备数据信息 json数组的方式
-     * @throws IOException
-     */
-    private JSONArray getSiteData() throws IOException {
-        // 获取设备session
-        OkHttpClient client = new OkHttpClient();
-        FormBody formBody = new FormBody.Builder()
-                .add("username", Global.username)
-                .add("password", Global.password)
-                .add("client_id", Global.clientId)
-                .add("client_secret", Global.clientSecret)
-                .add("scope", Global.scope)
-                .add("grant_type", Global.grant_type_login)
-                .build();
-        Request requestSession = new Request.Builder()
-                .url("https://account.flexem.com/core/connect/token")
-                .post(formBody)
-                .build();
-        Response responseSession = client.newCall(requestSession).execute();
-        if (responseSession.isSuccessful()) {
-            JSONObject sessionJson = JSONObject.parseObject(responseSession.body().string());
-            // 获取设备组信息
-            Request requestEquip = new Request.Builder().url("http://fbox360.com/api/client/box/grouped")
-                    .addHeader("Authorization", "Bearer " + sessionJson.getString("access_token"))
-                    .addHeader("X-FBox-ClientId", Global.signalrClientId)
-                    .build();
-            // 更新session用于站点与设备的请求
-            this.authorization = "Bearer " + sessionJson.getString("access_token");
-            Response responseEquip = client.newCall(requestEquip).execute();
-            if (responseEquip.isSuccessful()){
-                // 这里获取到的是站点名称的数据
-                JSONArray equipJson = JSONObject.parseArray(responseEquip.body().string());
-                responseEquip.body().close();
-                responseSession.body().close();
-                return equipJson;
-            }else{
-                throw new IOException("Unexpected code " + responseEquip);
-            }
-        } else {
-            throw new IOException("Unexpected code " + responseSession);
-        }
-    }
-
-    /**
-     * @desc 获取具体某个站点的设备数据
-     * @param siteNo
-     * @return
-     * @throws IOException
-     */
-    private JSONArray getEquipData (String siteNo) throws IOException {
-        String url = this.API_BASE_URL + "v2/box/dmon/grouped?boxNo=" + siteNo;
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(url)
-                .addHeader("Authorization", this.authorization)
-                .build();
-        Response response = client.newCall(request).execute();
-        if (response.isSuccessful()) {
-            JSONArray res = JSONArray.parseArray(response.body().string());
-            response.body().close();
-            return res;
-        } else {
-            throw new IOException("Unexpected code " + response);
-        }
     }
 }
